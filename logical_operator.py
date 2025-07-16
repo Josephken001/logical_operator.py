25c9968bab346f12af5f68aefcd8441799dffb80
Josephken001 <jk1505999@gmail.com>
2025-07-16 13:10:31 +0100

    my calculator

---
+++ logical_operator.py
@@ -0,0 +1,64 @@
+'''
+first_number = 4
+second_number = 5
+# Arithmetic operators
+# Plus +
+# Multiplication *
+# Subtraction 
+print(first_number)
+'''
+# A simple calculator app
+print("Welcome to my Super calculator")
+print("""**********************
+1. Addition
+2. Subtraction 
+3. Multiplication
+4. Division
+******************************""")
+print("Enter two numbers to add")
+# Prompts the user for a number 
+first_number = input("First Number:")
+# Prompts the user for  a number 
+second_number = input("Second Number:")
+sum = float(first_number) + float(second_number)
+print (f"{first_number} + {second_number} = {sum}")
+
+print("*******************")
+print("Subtraction")
+print("Enter two numbers to Subtract")
+# Prompts the user for a number
+first_number = input("First Number:")
+# Prompts the user for  a number
+second_number = input("Second Number:")
+sub = float(first_number) - float(second_number)
+print (f"{first_number} - {second_number} = {sub}")
+
+print("*******************")
+print("Multipliccation")
+print("Enter two numbers to Multiply")
+# Prompts the user for a number
+first_number = input("First Number:")
+# Prompts the user for  a number
+second_number = input("Second Number:")
+mul = float(first_number) * float(second_number)
+print (f"{first_number} * {second_number} = {mul}")
+
+print("*******************")
+print("Division")
+print("Enter two numbers to Divide")
+# Prompts the user for a number
+first_number = input("First Number:")
+# Prompts the user for  a number
+second_number = input("Second Number:")
+div = float(first_number) / float(second_number)
+print (f"{first_number} / {second_number} = {div}")
+
+print("*******************")
+print("Exponential")
+print("Enter two numbers to Exponent")
+# Prompts the user for a number
+first_number = input("First Number:")
+# Prompts the user for  a number
+second_number = input("Second Number:")
+expo = float(first_number) ** float(second_number)
+print (f"{first_number} ** {second_number} = {expo}")
